fe42448d05bbc6b83f3ae291ce4aadf024b7977c
Jagjeevan <83103469+JagjeevanAK@users.noreply.github.com>
2025-03-17 21:59:13 +0530

    Update run-integration.sh

--- scripts/run-integration.sh
+++ scripts/run-integration.sh
@@ -5,6 +5,7 @@ echo '🟡 - Waiting for database to be ready...'
 ./scripts/wait-for-it.sh "postgresql://postgres:mypassword@localhost:5432/postgres" -- 
 
 echo '🟢 - Database is ready!'
+
 npx prisma migrate dev --name init
 npm run test
 
