e51d807d7aa2a90eff06c041b97824f4765b808d
Jagjeevan <83103469+JagjeevanAK@users.noreply.github.com>
2025-03-17 21:50:59 +0530

    test

---
+++ .github/scripts/path-filter.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Filter non-markdown and non-docs files
+CODE_FILES=$(echo "$1" | grep -E -v '\.md$|^docs/')
+if [ -n "$CODE_FILES" ]; then
+  echo "code_modified=true" >> "$GITHUB_OUTPUT"
+else
+  echo "code_modified=false" >> "$GITHUB_OUTPUT"
+fi
